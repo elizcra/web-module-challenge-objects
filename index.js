801fc0e3dc8c689c760a95a5967ad65b9f3837ff
Elizabeth Crane <elizabeth.anne.crane@gmail.com>
2020-06-25 19:01:53 -0400

    mvp task 1 a complete

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakf
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
 function createMenuItem(name, cost, category){
-    /* Code here */
+  let menuItem = {
+    name: name,
+    cost: cost,
+    category: category
+  }
+  console.log(menuItem);
 }
+
+//console.log(createMenuItem("Test", 42, "Control"));
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
